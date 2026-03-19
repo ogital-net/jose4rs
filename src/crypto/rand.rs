5b9198313712ee97b1adafa1c873b7f4bf59c0fb
Spencer Thomason <spencer@latigo.net>
2026-03-19 09:06:50 -0700

    Delegate rand functions to the crypto lib

--- src/crypto/rand.rs
+++ src/crypto/rand.rs
@@ -1,55 +1,21 @@
+#[cfg(feature = "aws-lc")]
+use aws_lc_sys::RAND_bytes;
+#[cfg(feature = "boring")]
+use boring_sys::RAND_bytes;
+
 use super::mem;
 
-#[cfg(target_os = "linux")]
-#[link(name = "c")]
-unsafe extern "C" {
-    unsafe fn getrandom(buf: *mut u8, buflen: usize, flags: core::ffi::c_uint) -> isize;
-}
-
-#[cfg(target_os = "linux")]
 pub(crate) fn rand_bytes_buf(mut dst: impl AsMut<[u8]>) {
-    let dst = dst.as_mut();
-    let len = dst.len();
-
-    unsafe {
-        let res = getrandom(dst.as_mut_ptr(), len, 0);
-        // this call is infallible on a modern linux kernel, but just in case something changes.
-        if res < 0 {
-            panic!(std::io::Error::last_os_error());
-        } else if res != len as isize {
-            panic!("Unable to generate {} random bytes", len);
-        }
+    let buf = dst.as_mut();
+    if buf.is_empty() {
+        return;
     }
-}
-
-#[cfg(target_os = "macos")]
-unsafe extern "C" {
-    unsafe fn CCRandomGenerateBytes(bytes: *mut u8, count: usize) -> i32;
-}
-
-#[cfg(target_os = "macos")]
-pub(crate) fn rand_bytes_buf(mut dst: impl AsMut<[u8]>) {
-    let dst = dst.as_mut();
-
-    unsafe {
-        assert!(
-            0 == CCRandomGenerateBytes(dst.as_mut_ptr(), dst.len()),
-            "Unable to generate random bytes"
-        );
-    }
-}
-
-#[cfg(not(any(target_os = "linux", target_os = "macos")))]
-pub(crate) fn rand_bytes_buf(mut dst: impl AsMut<[u8]>) {
-    use aws_lc_sys::RAND_bytes;
-
-    let dst = dst.as_mut();
-
-    unsafe {
-        assert!(
-            1 == RAND_bytes(dst.as_mut_ptr(), dst.len()),
-            "Unable to generate random bytes"
-        );
+    
+    let result = unsafe { RAND_bytes(buf.as_mut_ptr(), buf.len()) };
+    
+    // RAND_bytes returns 1 on success, 0 or -1 on failure
+    if result != 1 {
+        panic!("RAND_bytes failed");
     }
 }
 
